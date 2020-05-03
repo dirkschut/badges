de0671432ecfa9b550c64844568c4486615d5437
Dirk Schut <dschut@outlook.com>
2020-05-03 20:58:19 +0200

    Added the first steps of the badge category system

--- logic.js
+++ logic.js
@@ -1,29 +1,40 @@
 const TYPE_BADGR = 0;
 const TYPE_LOCAL = 1;
 
+const CAT_FEATURED = "Featured";
+const CAT_OPENBADGES = "Open Badges";
+const CAT_TEST = "Test";
+
 //The data of all the badges
 const badgeData = {
     "Badge: Badgr Web Explorer": {
         type: TYPE_BADGR,
         src: "https://api.badgr.io/public/assertions/nhztqxJeT1OaiM8-KhAkLA?embedVersion=1&amp;embedWidth=330&amp;embedHeight=186&amp;identity__email=dschut%40outlook.com",
+        categories: [CAT_FEATURED, CAT_OPENBADGES],
     },
     "test1": {
         type: TYPE_LOCAL,
+        categories: [CAT_TEST],
     },
     "test2": {
         type: TYPE_LOCAL,
+        categories: [CAT_TEST],
     },
     "test3": {
         type: TYPE_LOCAL,
+        categories: [CAT_TEST, CAT_FEATURED],
     },
     "test5": {
         type: TYPE_LOCAL,
+        categories: [CAT_TEST],
     },
     "test6": {
         type: TYPE_LOCAL,
+        categories: [CAT_TEST],
     },
     "test7": {
         type: TYPE_LOCAL,
+        categories: [CAT_TEST],
     },
 };
 
@@ -34,12 +45,20 @@ function renderBadges(badgesToRender){
     for(badgeName in badgesToRender){
         switch(badgesToRender[badgeName].type){
             case TYPE_BADGR:
-                badgesString += "<iframe class='col-md-4 myBadge' src='" + badgesToRender[badgeName].src + "' title='" + badgeName + "'></iframe>";
+                badgesString += "<div class='col-md-4 myBadge'><iframe src='" + badgesToRender[badgeName].src + "' title='" + badgeName + "'></iframe>";
                 break;
             case TYPE_LOCAL:
-                badgesString += "<div class='col-md-4 myBadge testBadge'><h3>" + badgeName + "</h3></div>";
+                badgesString += "<div class='col-md-4 myBadge testBadge'><h3>" + badgeName + "</h3>";
                 break;
         }
+
+        badgesString += "<ul class='badgesCategories'>"
+        for(category in badgesToRender[badgeName].categories){
+            badgesString += "<li>" + badgesToRender[badgeName].categories[category] + "</li>";
+        }
+        badgesString += "</ul>";
+
+        badgesString += "</div>";
     }
 
     badgesString += "</div>";
